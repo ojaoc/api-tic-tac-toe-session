a3da3ed787b9358e1ee50878b2e177b253210471
João Correia <correia.jafonso@gmail.com>
2021-12-08 20:30:29 +0000

    Setup Socket.IO

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
 const port = 8080; // default port to listen
 
 // define a route handler for the default home page
@@ -7,8 +12,15 @@ app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("message", (message) => {
+    console.log(message);
+    io.emit("message", message);
+  });
+});
+
 // start the Express server
-app.listen(port, () => {
-  // tslint:disable-next-line:no-console
-  console.log(`server started at http://localhost:${port}`);
+server.listen(port, () => {
+  console.log(`listening on *:${port}`);
 });
