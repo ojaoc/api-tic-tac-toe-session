eb4b3f67242b5f8b423046a8157164c64dcba4d8
João Correia <correia.jafonso@gmail.com>
2021-12-13 23:31:06 +0000

    Ignore 'req' arg in app.get("/")

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const io = new Server(server, { cors: { origin: "*" } });
 const port = 8080; // default port to listen
 
 // define a route handler for the default home page
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.send("Hello world!");
 });
 
