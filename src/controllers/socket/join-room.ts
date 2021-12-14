3549cda5e958f6ef30e9076fc7352cb34119a3fc
João Correia <joao.correia@contisystems.pt>
2021-12-14 16:50:15 +0000

    Don't understand why closure that i'm passing as callback isn't changing value

--- src/controllers/socket/join-room.ts
+++ src/controllers/socket/join-room.ts
@@ -1,31 +1,35 @@
-import { EventHandlerArgs } from "src/ts/socket-interfaces";
+import { EventHandlerArgs, RoomResponse } from "src/ts/socket-interfaces";
 import { v4 as uuidv4 } from "uuid";
 
 export const joinRoomController =
-  ({ io, socket, rooms }: EventHandlerArgs) =>
-  (roomId: string): void => {
-    if (!roomId) {
-      socket.join(uuidv4());
-      const [createdSocketRoomId] = socket.rooms;
-      io.emit("room:created", {
-        id: createdSocketRoomId,
-        isFull: rooms.get(createdSocketRoomId).size >= 2,
-      });
-    } else {
-      if (!rooms.get(roomId)) {
-        socket.emit("message", "Room not found.");
-        socket.disconnect(true);
-        return;
-      } else if (rooms.get(roomId)?.size >= 2) {
-        socket.emit("message", "Room already full.");
-        socket.disconnect(true);
-        return;
-      } else {
-        socket.join(roomId);
-        io.emit("room:updated", {
-          id: roomId,
-          isFull: rooms.get(roomId).size >= 2,
-        });
-      }
-    }
-  };
+    ({ io, socket, rooms }: EventHandlerArgs) =>
+    (roomId: string, cb: (arg0: RoomResponse) => void): void => {
+        if (!roomId) {
+            socket.join(uuidv4());
+            const [createdSocketRoomId] = socket.rooms;
+            const roomObj: RoomResponse = {
+                id: createdSocketRoomId,
+                isFull: rooms.get(createdSocketRoomId).size >= 2,
+            };
+            cb(roomObj);
+            io.emit("room:created", roomObj);
+        } else {
+            if (!rooms.get(roomId)) {
+                socket.emit("message", "Room not found.");
+                socket.disconnect(true);
+                return;
+            } else if (rooms.get(roomId)?.size >= 2) {
+                socket.emit("message", "Room already full.");
+                socket.disconnect(true);
+                return;
+            } else {
+                socket.join(roomId);
+                const roomObj: RoomResponse = {
+                    id: roomId,
+                    isFull: rooms.get(roomId).size >= 2,
+                };
+                cb(roomObj);
+                io.emit("room:updated", roomObj);
+            }
+        }
+    };
